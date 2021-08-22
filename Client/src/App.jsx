fe874d1c42844f220497fba72d21fb2874180e2c
jamie tardi <66372574+jamieTardi@users.noreply.github.com>
2021-08-22 10:51:45 +0100

    restructure

---
+++ Client/src/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+const App = () => {
+	return <div>test</div>;
+};
+
+export default App;
--- Client/wowroster/src/App.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const App = () => {
-	return <div>test</div>;
-};
-
-export default App;
