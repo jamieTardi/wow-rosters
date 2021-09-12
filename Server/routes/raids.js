7fabe50782cd8c190759fdbf5336a72bbd793bd7
jamie tardi <66372574+jamieTardi@users.noreply.github.com>
2021-09-12 16:46:06 +0100

    slight changes to the back end

--- Server/routes/raids.js
+++ Server/routes/raids.js
@@ -11,8 +11,8 @@ import auth from '../middleware/auth.js';
 const router = express.Router();
 
 router.get('/', getRaids);
-router.post('/', auth, createRaid);
-router.patch('/:id', auth, updateRaid);
-router.delete('/:id', auth, deletedRaid);
+router.post('/', createRaid);
+router.patch('/:id', updateRaid);
+router.delete('/:id', deletedRaid);
 
 export default router;
